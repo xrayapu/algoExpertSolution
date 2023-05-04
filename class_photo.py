82cf6517bae68056f781f56faa7b50e933b9a510
apu <40669057+xrayapu@users.noreply.github.com>
2023-05-04 16:47:06 +0600

    Update class_photo.py

--- class_photo.py
+++ class_photo.py
@@ -1,3 +1,5 @@
+# type 1
+
 def sol( arr1, arr2):
     arr1.sort()
     arr2.sort()
@@ -30,3 +32,25 @@ def sol( arr1, arr2):
 # b=[9,5,2,4,5]
 
 # print(sol(a,b))
+
+# type 2
+
+def sol(ar1,ar2):
+    ar1.sort()
+    ar2.sort()
+    check = 'red' if ar1[0] > ar2[0] else 'blue'
+    if len(ar2) != len(ar1): return False
+    else:
+        i=1
+        while(i < len(ar2)) :
+            if check == 'red':
+                if ar1[i] <= ar2[i]:
+                    return False
+                
+            else :
+                if ar1[i] >= ar2[i]:
+                    return False
+                
+            i+=1
+
+    return True
