32045792dc20b9dd56429b2ddd79b351903ca563
apu <40669057+xrayapu@users.noreply.github.com>
2023-05-29 18:37:19 +0600

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+# binary search using iterative way
+
+### implementation
+
+def sol(arr, tar):
+    
+    i,j=0, len(arr)-1
+    while i<= j :
+        mid = (i+j)//2
+        if arr[mid] ==tar:
+            return mid
+        elif arr[mid] > tar:
+            j = mid -1
+
+        else : i = mid +1
+
+    return -1
