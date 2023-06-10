5e3fa078459fe39b2a4039ffbabf7cf8adad995d
apu <40669057+xrayapu@users.noreply.github.com>
2023-06-10 17:55:21 +0600

    Create caesor_encryptor.py
    if they asked without using ord function ,it will be very boring.

---
+++ caesor_encryptor.py
@@ -0,0 +1,19 @@
+# using ord function 
+def sol(sr, n):
+    arr='' # []
+    if n > 26:
+        n = n % 26 # for edge case 
+    for i in sr:       
+        nm = ord(i)        
+        nm +=n
+        if nm > 122:
+            nm = 96 + nm %122           
+        arr += chr(nm)
+
+    return arr # return "".join(arr)
+# time O(n) | space O(n)
+
+# a='yz' 
+# n=2
+# print(sol(a,n))
+
