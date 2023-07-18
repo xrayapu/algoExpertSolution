0582a5faecfcb1a3a18b944dceed290a2c42bce4
apu <40669057+xrayapu@users.noreply.github.com>
2023-07-18 12:44:48 +0600

    Create array_of_products_7.py

---
+++ medium_problems/array_of_products.py
@@ -0,0 +1,34 @@
+# array of products [5,1,4,2]=>[8,40,10,20]
+
+
+# using division
+# type 1: time O(n) | space O(1)
+
+def sol(arr):
+    ans = 1
+    for i in arr:
+        ans *= i
+
+    for i in range(len(arr)):
+        arr[i] = ans // arr[i]
+
+    return arr
+
+# brute force
+# type 2 : time O(n^2) | space O(n)
+
+
+def sol(arr):
+    p = []
+    for i in range(len(arr)):
+        ans = 1
+        for j in range(len(arr)):
+            if i != j:
+                ans *= arr[j]
+
+        p.append(ans)
+
+    return p
+
+
+# type 3: time O(n) | space O()
