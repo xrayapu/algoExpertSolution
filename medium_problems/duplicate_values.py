ec701353cd6336ca3293efadf17df95920e50024
apu <40669057+xrayapu@users.noreply.github.com>
2023-07-19 12:22:25 +0600

    Update duplicate_values_8.py

--- medium_problems/duplicate_values.py
+++ medium_problems/duplicate_values.py
@@ -1,19 +1,33 @@
-# type: 1
-def sol(nums):
-    p = nums
-    p.sort()
-    for i in range(len(p)):
-        if p[i] == p[i + 1]:
-            return p[i]
-#time: O(nlogn) | space O(n)
-#type:2
+# # type: 1
+# def sol(nums):
+#     p = nums
+#     p.sort()
+#     for i in range(len(p)):
+#         if p[i] == p[i + 1]:
+#             return p[i]
+# #time: O(nlogn) | space O(n)
+# #type:2
 
-from collections import Counter
-def sol(nums):  
-    x = Counter(nums)
-    for num, val in x.items():
-        if val > 1:
+# from collections import Counter
+# def sol(nums):  
+#     x = Counter(nums)
+#     for num, val in x.items():
+#         if val > 1:
+#             return num
+
+# #time O(n) | space O(n)
+# type 1 and type 2 leetcode problem solutuion
+
+
+
+
+# type 3 for algoexpert solution
+def sol(arr):
+    m = {}
+    for i in range(len(arr)):
+        num = arr[i]
+        if num in m.values():
             return num
 
-
+        m[i] = num
 #time O(n) | space O(n)
