db5dd473c5c746338685f417326b7501f53f13ff
apu <40669057+xrayapu@users.noreply.github.com>
2023-07-23 16:14:22 +0600

    Create interval_mid9.py

---
+++ medium_problems/interval_mid9.py
@@ -0,0 +1,24 @@
+# merge overlapping intervals
+def sol(arr):
+    arr.sort()
+    ans = []
+
+    inta = arr[0]
+    
+    ans.append(inta)
+    
+    for next in arr:
+        _, ans0 = inta
+
+        ans1, ans2 = next
+       
+        if ans0 >= ans1:
+            inta[1] = max(ans0, ans2)
+        else:
+            inta = next
+            ans.append(inta)
+    return ans
+
+
+# print(sol([[1, 3], [2, 6], [8, 10], [15, 18]]))
+#time O(nlogn) and space O(n)
