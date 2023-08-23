7c86dec6476005e9924888711b314d7b178f8f64
apu <40669057+xrayapu@users.noreply.github.com>
2023-08-23 13:03:16 +0600

    Create lesson38.py

---
+++ medium_problems/lesson38.py
@@ -0,0 +1,18 @@
+# largest value in the tree 
+# time O(n) | space O(n)
+
+def it(node, t):
+    def tree_trvrsal(node):
+        if not node:
+            return
+
+        tree_trvrsal(node.left)
+        ans.append(node.val)
+        tree_trvrsal(node.right)
+
+        return ans
+
+    ans = []
+    tree_trvrsal(node)
+    
+    return ans[-t ]
