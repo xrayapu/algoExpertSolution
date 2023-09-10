97e6ed9079f31b1eac98d4909b8f67ce23df7fc1
apu <40669057+xrayapu@users.noreply.github.com>
2023-09-10 23:40:20 +0600

    Create pro3_square.py

---
+++ easy/pro3_square.py
@@ -0,0 +1,36 @@
+def sol(arr):
+    ans = []
+    for num in arr:
+        num = num * num
+        ans.append(num)
+
+    ans.sort()
+    return ans
+
+# time O(nlogn) | space O(n)
+  
+# print(sol([-3, 1, 5]))
+
+
+
+# optimal solution , if you get sorted array then you should try to solve it in O(n) time.
+
+def sol(arr):
+    i = 0
+    j = len(arr) - 1
+    ans = [0 for _ in arr]
+    # print(len(ans))
+    for idx in reversed(range(len(arr))):
+        small = arr[i]
+        large = arr[j]
+        if abs(small) > abs(large):
+            ans[idx] = small**2
+            i += 1
+        else:
+            ans[idx] = large**2
+            j -= 1
+
+    return ans
+
+# time O(n) | space O(n)
+print(sol([-7, -3, 1, 5]))
