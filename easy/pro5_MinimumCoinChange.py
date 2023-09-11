a1bc912430cd889cd7a206de1bec22905f0837fc
apu <40669057+xrayapu@users.noreply.github.com>
2023-09-11 21:07:07 +0600

    Update pro5_MinimumCoinChange.py

--- easy/pro5_MinimumCoinChange.py
+++ easy/pro5_MinimumCoinChange.py
@@ -1,3 +1,5 @@
+# you are given some coin , return the minimum coin that can't created by all of this coins ! 
+
 def sol(arr):
     arr.sort()
     make = 0
@@ -13,3 +15,8 @@ def sol(arr):
                 make += arr[idx]
 
     return make
+# a=[1, 4, 1] output: 3
+# a=[1,1,3] output:6
+# a=[1,2,3] output: 7
+# a= [5,7,1,3,1,2,22] output: 20
+# print(sol(a))
