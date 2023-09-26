3f15f40678d523a6d4b578466a323592d4288dd5
apu <40669057+xrayapu@users.noreply.github.com>
2023-09-26 10:45:35 +0600

    Update lesson6_bst_close_value.py

--- lesson6_bst_close_value.py
+++ lesson6_bst_close_value.py
@@ -41,3 +41,54 @@ def sol(root, tar, closeval):
             break
 
     return closeval
+
+# full code
+
+# class node:
+#     def __init__(self, val):
+#         self.right = None
+#         self.left = None
+#         self.info = val
+
+
+# def insert(tree, val):
+#     if not tree:
+#         return node(val)
+#     if tree.info > val:
+#         tree.left = insert(tree.left, val)
+
+#     else:
+#         tree.right = insert(tree.right, val)
+
+#     return tree
+
+
+# def helper(root, tar, comp):
+#     if not root:
+#         return comp
+#     if abs(tar - comp) > abs(tar - root.info):
+#         comp = root.info
+#     if root.info == tar:
+#         return comp
+#     elif root.info > tar:
+#         return helper(root.left, tar, comp)
+#     else:
+#         return helper(root.right, tar, comp)
+
+
+# def sol(root, tar):
+#     return helper(root, tar, comp=float("inf"))
+
+
+# tree = node(10)
+
+# tree = insert(tree, 5)
+# tree = insert(tree, 2)
+# tree = insert(tree, 5)
+# tree = insert(tree, 1)
+# tree = insert(tree, 15)
+# tree = insert(tree, 13)
+# tree = insert(tree, 22)
+# tree = insert(tree, 14)
+
+# print(sol(tree, 12))
