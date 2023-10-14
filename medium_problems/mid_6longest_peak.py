f06421308812e9dc42a1f3729af907e3cdb30e80
apu <40669057+xrayapu@users.noreply.github.com>
2023-10-14 23:12:39 +0600

    Update mid_6longest_peak.py

--- medium_problems/mid_6longest_peak.py
+++ medium_problems/mid_6longest_peak.py
@@ -6,7 +6,7 @@ def sol(arr):
             left= right= i
             while left >0 and arr[left] >arr[left-1]:
                 left-=1
-            while right < len(arr) and arr[right] > arr[right+1]:
+            while right < len(arr)-1 and arr[right] > arr[right+1]: # right+1 <len(arr) , comment code is ok but takes much time ! 
                 right+=1
             val=right-left+1 # idx number from up and 1 for idx start with 0
             ans= max(ans, val)
