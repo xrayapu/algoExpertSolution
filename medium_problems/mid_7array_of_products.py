27a809cff0c5aa4bc6a1f268475d7c3d8edcb7cb
apu <40669057+xrayapu@users.noreply.github.com>
2023-10-16 23:19:00 +0600

    Update mid_7array_of_products.py

--- medium_problems/mid_7array_of_products.py
+++ medium_problems/mid_7array_of_products.py
@@ -1,7 +1,7 @@
 # array of products [5,1,4,2]=>[8,40,10,20]
 
 
-# using division 
+# using division if input has no 0
 # type 1: time O(n) | space O(1)
 
 def sol(arr):
@@ -32,3 +32,23 @@ def sol1(arr):
 
 
 # type 3: time O(n) | space O()
+
+#left=[1,5,5,20]
+#right=[8,8,2,1]
+# ans=[8,40,10,20]
+def sol(arr):
+    ans = [1] * len(arr)
+    left = [1] * len(arr)
+    right = [1] * len(arr)
+    leftval = 1
+    for i in range(len(arr)):
+        left[i] = leftval
+        leftval *= arr[i]
+    ritval = 1
+    for i in reversed(range(len(arr))):
+        right[i] = ritval
+        ritval *= arr[i]
+    for i in range(len(arr)):
+        ans[i] = left[i] * right[i]
+
+    return ans
