9854d746068eb024ac1987f123d874e31ebb994d
apu <40669057+xrayapu@users.noreply.github.com>
2023-11-04 19:40:24 +0600

    Update mid_19heightBalancedBinaryTree.py

--- medium_problems/mid_19heightBalancedBinaryTree.py
+++ medium_problems/mid_19heightBalancedBinaryTree.py
@@ -30,7 +30,7 @@ def balancedTree(root):
           return [True, 0]
         left = sol(root.left)
         right= sol(root.right)
-        balanced= left[0] and right[0] and abs(left[1] - right[1]<= 1)
+        balanced= left[0] and right[0] and abs(left[1] - right[1])<= 1
         return [balanced , 1+max(left[1], right[1])]
 
     return sol(root)[0]
