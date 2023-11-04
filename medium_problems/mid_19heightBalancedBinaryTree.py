ea31d623daa8eae9539d1973b8009ff12aca0d33
apu <40669057+xrayapu@users.noreply.github.com>
2023-11-04 19:31:36 +0600

    Create mid_19heightBalancedBinaryTree.py

---
+++ medium_problems/mid_19heightBalancedBinaryTree.py
@@ -0,0 +1,24 @@
+
+
+# A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+
+"""
+              3
+           /   \
+          4     2
+        /  \   / \
+       1   5  7   9      output => true
+                 / \
+                8  11 
+
+             3
+           /   \
+          4     2
+               / \
+              7   9    output => false
+                   \
+                   11 
+"""
+"""
+
