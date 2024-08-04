b13456bf27f9b8cb8bf9b1ac2b3cbac5266063ed
apu <40669057+xrayapu@users.noreply.github.com>
2024-08-04 20:39:14 +0600

    Update problem1_2sum.py

--- easy/problem1_2sum.py
+++ easy/problem1_2sum.py
@@ -1,3 +1,4 @@
+#type 1:
 def sol(arr, tar):
     m = {}
 
@@ -8,3 +9,24 @@ def sol(arr, tar):
 
         else:
             return temp, num
+
+# time O(n) | space O(n)
+
+#type 2:
+
+def sol(arr, tar):
+    arr.sort() # sort it
+    i,j=0,len(arr)-1
+    while i <j: # just 1st and last compaire !
+        it=arr[i]+arr[j]
+        if it < tar: # if it is smaller then target we just move i
+            i+=1
+        elif it> tar: # if it is greater then target we just move j
+            j-=1
+        else: # target= sum of 2 number
+            return [arr[i], arr[j]]
+        
+    return [-1,-1]
+    
+# time o(nlogn) | space O(1)
+print(sol([1,7,3,10],10))
